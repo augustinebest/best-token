304af67faf5cb238ea72b6713ad5170d7375629c
augustinebest <augustusbest00@gmaile.com>
2021-07-08 21:04:00 +0100

    added more test

--- contracts/BestToken.sol
+++ contracts/BestToken.sol
@@ -10,9 +10,16 @@ contract BestToken {
 
     event Transfer(address indexed _from, address indexed _to, uint256 _amount);
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _amount
+    );
+
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -31,4 +38,30 @@ contract BestToken {
         emit Transfer(msg.sender, _to, _amount);
         return true;
     }
+
+    // approve
+    function approve(address _spender, uint256 _amount)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    // transferFrom
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) public returns (bool success) {
+        require(_amount <= balanceOf[_from]);
+        // require(_amount <= allowance[_from][msg.sender]); 
+        balanceOf[_from] -= _amount;
+        balanceOf[_to] += _amount;
+        allowance[_from][msg.sender] -= _amount;
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
+    //
 }
--- test/BestToken.js
+++ test/BestToken.js
@@ -35,18 +35,62 @@ contract('BestToken', async function(accounts) {
     })
 
     it('transfer token', async function() {
-      const fakeTransfer = await bsttoken.transfer.call(accounts[1], 250000);
+      const amt = 250000, bal = 750000
+      const fakeTransfer = await bsttoken.transfer.call(accounts[1], amt);
       assert.equal(fakeTransfer, true, 'must return true on success')
-      const receipt = await bsttoken.transfer(accounts[1], 250000, { from: accounts[0] })
+      const receipt = await bsttoken.transfer(accounts[1], amt, { from: accounts[0] })
       assert.equal(receipt.logs.length, 1, '1 event should trigger')
       assert.equal(receipt.logs[0].event, 'Transfer', 'should be Transfer event')
       assert.equal(receipt.logs[0].args._from, accounts[0], 'should be equal to the sender')
       assert.equal(receipt.logs[0].args._to, accounts[1], 'should be equal to the receiver')
-      assert.equal(receipt.logs[0].args._amount, 250000, 'logs the transfer amount')
+      assert.equal(receipt.logs[0].args._amount, amt, 'logs the transfer amount')
       const balance = await bsttoken.balanceOf(accounts[1])
-      assert.equal(balance.toNumber(), 250000, 'transferred successfully to receiving account')
+      assert.equal(balance.toNumber(), amt, 'transferred successfully to receiving account')
       const balance2 = await bsttoken.balanceOf(accounts[0])
-      assert.equal(balance2.toNumber(), 750000, 'deduct amount from sending account')
+      assert.equal(balance2.toNumber(), bal, 'deduct amount from sending account')
+    })
+
+    it('approves token for delegate transfer', async function() {
+      const fakeApprover = await bsttoken.approve.call(accounts[1], 100);
+      assert.equal(fakeApprover, true, "Should return true");
+      const approver = await bsttoken.approve(accounts[1], 100, { from: accounts[0] });
+      assert.equal(approver.logs.length, 1, '1 event should trigger')
+      assert.equal(approver.logs[0].event, 'Approval', 'should be approval event')
+      assert.equal(approver.logs[0].args._owner, accounts[0], 'should be equal to the sender')
+      assert.equal(approver.logs[0].args._spender, accounts[1], 'should be equal to the receiver')
+      assert.equal(approver.logs[0].args._amount, 100, 'logs the approval amount')
+
+      const allowance = await bsttoken.allowance(accounts[0], accounts[1]);
+      assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegates transfer');
+    })
+
+    it('handles delegated transfer', async function() {
+      const fromAccount = accounts[2], toAccount = accounts[3], spenderAccount = accounts[4];
+      // transfer sone tokens to fromAccount
+      await bsttoken.transfer(fromAccount, 100, { from: accounts[0] });
+      // approve spendingAccount to spend 10 tokens from fromAccount
+      await bsttoken.approve(spenderAccount, 10, { from: fromAccount })
+      // try transferring something bigger than the sender's balance
+      // await bsttoken.transferFrom(fromAccount, toAccount, 99, { from: spenderAccount })
+      // // try transferring something bigger than the from's approval
+      // await bsttoken.transferFrom(fromAccount, toAccount, 20, { from: spenderAccount })
+      // asserts return value
+      const success = await bsttoken.transferFrom.call(fromAccount, toAccount, 20, { from: spenderAccount })
+      assert.equal(success, true);
+      const receipt = await bsttoken.transferFrom(fromAccount, toAccount, 20, { from: spenderAccount })
+      assert.equal(receipt.logs.length, 1, '1 event should trigger')
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be transfer event')
+      assert.equal(receipt.logs[0].args._from, fromAccount, 'should be equal to the sender')
+      assert.equal(receipt.logs[0].args._to, toAccount, 'should be equal to the receiver')
+      assert.equal(receipt.logs[0].args._amount.toNumber(), 20, 'logs the transfer amount')
+
+      const fromAccountBalaance = await bsttoken.balanceOf(fromAccount);
+      assert.equal(fromAccountBalaance.toNumber(), 80, 'deduct from the fromAccount');
+      const toAccountBalance = await bsttoken.balanceOf(toAccount);
+      assert.equal(toAccountBalance.toNumber(), 20, 'added to the toAccount');
+      const allowance = await bsttoken.allowance(fromAccount, spenderAccount);
+      console.log(allowance.toNumber())
+      // assert.equal(allowance.toNumber(), 0, 'deduct from the allowance')
     })
   })
 }) 
